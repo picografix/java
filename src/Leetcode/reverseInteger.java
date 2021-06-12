b3f484cfdab149280ddfaf6b0c91f61bcf1919b3
picografix <picografix@gmail.com>
2021-06-12 23:36:00 +0530

    reverseInteger

--- src/Leetcode/reverseInteger.java
+++ src/Leetcode/reverseInteger.java
@@ -2,3 +2,16 @@ package Leetcode;
 
 public class reverseInteger {
 }
+class SolutionInteger {
+    public int reverse(int x) {
+        long ans = 0;
+        while(x  !=0){
+            ans = 10*ans+((x%10))%10;
+            x=x/10;
+        }
+        if(ans>= 2147483647L || ans<= -2147483648L){
+            return 0;
+        }
+        return (int) ans;
+    }
+}
